c089c71eafcc88aa65489cbc255b47fe02dfac9a
Oskar Akerlund <oskar.akerlund@wikinggruppen.se>
2022-04-15 18:25:09 +0200

    Add storage (DynamoDB, S3 bucket)

--- src/lambda.js
+++
@@ -1,7 +0,0 @@
-export async function handler(event) {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello, Woooorld! Your request was received at ${event.requestContext.time}.`,
-  };
-}
